0b322f7695307441d3edd21267341c7754e8459c
Sanket Bagad <alencolins@gmail.com>
2021-12-19 13:12:38 +0530

    Setting up the structure

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -16,7 +16,7 @@ const NavBar = () => {
   </Dropdown.Toggle>
 
   <Dropdown.Menu>
-    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+    <Dropdown.Item href="/addProduct">Add Product</Dropdown.Item>
     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
   </Dropdown.Menu>
